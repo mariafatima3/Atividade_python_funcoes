61661126eddd95b5337e63058ce9858c98c0c7cd
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2026-04-08 18:16:39 +0000

    quest_10.py

---
+++ quest_10.py
@@ -0,0 +1,9 @@
+# 10) Escreva um programa que solicite ao usuário duas strings e verifique se elas são iguais. Imprima uma mensagem informando o resultado da comparação.
+
+string1 = input("Digite a primeira string: ")
+string2 = input("Digite a segunda string: ")
+
+if string1 == string2:
+    print("As strings são iguais.")
+else:
+    print("As strings são diferentes.")
