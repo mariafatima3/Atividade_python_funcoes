556d52748cac690cb8cb0d254e473ac2f720f6c7
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2026-04-08 17:50:46 +0000

    quest_2.py

---
+++ quest_2.py
@@ -0,0 +1,4 @@
+# 2) Discuta a importância das anotações de tipo (type hints) na linguagem Python e explique como elas podem ajudar no desenvolvimento de código.
+
+"As type hints são importantes porque aumentam a segurança do código (via análise estática), melhoram drasticamente a manutenibilidade e a comunicação entre desenvolvedores, "
+"e potencializam as ferramentas de autocompletar das IDEs, tornando o desenvolvimento mais rápido e menos propenso a bugs bobos de tipagem."
