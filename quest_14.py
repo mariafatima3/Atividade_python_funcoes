4d553fb9a5089a44d729f5008a4c5ff668038b6f
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2026-04-08 18:29:19 +0000

    quest_14.py

---
+++ quest_14.py
@@ -0,0 +1,9 @@
+# 14 Um foguete atinge uma velocidade constante de 1500 m/s. Se ele voar durante 40 segundos, qual é a altitude que o foguete alcançará?
+
+velocidade = 1500
+tempo = 40
+
+distancia = velocidade * tempo
+
+print(f"Altitude em metros: {distancia} m")
+print(f"Altitude em km: {distancia / 1000} km")
