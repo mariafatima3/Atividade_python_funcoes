a916f9c0ff301d033e630fcd35e44bb1ef1d5c77
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2026-04-08 17:49:22 +0000

    quest_1.py

---
+++ quest_1.py
@@ -0,0 +1,5 @@
+# 1) Descreva o processo de criação de um ambiente virtual em Python e por que isso pode ser útil em projetos com diferentes dependências.
+
+"Ambientes virtuais isolam as bibliotecas de cada projeto, evitando conflitos de versões e mantendo o sistema limpo. "
+"O processo utiliza o comando python -m venv para criação, seguido da ativação pelo terminal para direcionar o uso do Python àquela pasta."
+" Isso garante a reprodutibilidade, permitindo que diferentes projetos funcionem com dependências específicas simultaneamente."
