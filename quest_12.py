247799a641e183d68badf042ccaaff51feefbc5f
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2026-04-08 18:22:04 +0000

    quest_12

---
+++ quest_12.py
@@ -0,0 +1,9 @@
+# 12) Escreva um programa que solicite ao usuário a sua idade e verifique se ele é maior de idade (idade igual ou superior a 18 anos).
+# Imprima uma mensagem informando o resultado.
+
+idade = int(input("Digite a sua idade: "))
+
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
